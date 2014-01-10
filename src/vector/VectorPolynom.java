b768d39f163defc61565e5045b180e52c424c646
CedricHanebaum@cae <Cedric.Hanebaum@cae.de>
2014-01-10 14:43:37 +0100

    implemented scalarProd, getNorm, getWinkel, projeziere. Still buggy.

--- src/vector/VectorPolynom.java
+++ src/vector/VectorPolynom.java
@@ -8,6 +8,8 @@ public class VectorPolynom implements Vector {
 		this.koeffizienten = koeffizienten;
 	}
 
+	
+	//FIXME this objekt wird verändert
 	@Override
 	public Vector add(Vector v2) {
 		VectorPolynom vp;
@@ -42,31 +44,41 @@ public class VectorPolynom implements Vector {
 
 	@Override
 	public double scalarProd(Vector v2) {
-		return 0;
+		try {
+			return this.getMultIntegral((VectorPolynom) v2);
+		} catch (ClassCastException e){
+			throw new IllegalArgumentException("An Error occured. It´s probably located between chair and keyboard.");
+		}
 	}
 
 	@Override
 	public double getNorm() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.sqrt(this.scalarProd(this));
 	}
 
 	@Override
 	public double getWinkel(Vector v2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return Math.acos(this.scalarProd(v2) / this.getNorm() * v2.getNorm());
 	}
 
 	@Override
 	public Vector projeziereV1AufV2(Vector v2) {
-		// TODO Auto-generated method stub
-		return null;
+		return v2.mult(this.scalarProd(v2) / v2.scalarProd(v2));
 	}
 	
 	public int getGrad(){
 		return koeffizienten.length - 1;
 	}
 	
+	private double getMultIntegral(VectorPolynom v2){
+		double ret = 0;
+		for(double i = -1; i < 1; i += 0.1){
+			ret += 0.1 * (this.getValueAt(i) * v2.getValueAt(i));
+		}
+		return ret;
+	}
+	
+	@SuppressWarnings("unused")
 	private double getIntegral(){
 		double ret = 0;
 		for(double i = -1; i < 1; i += 0.1){
@@ -79,6 +91,14 @@ public class VectorPolynom implements Vector {
 		double ret = 0;
 		for(int i = 0; i < koeffizienten.length; i++){
 			ret += koeffizienten[i]*Math.pow(x, i);
+		}
+		return ret;
+	}
+	
+	public String toString(){
+		String ret = "";
+		for(int i = 0; i < koeffizienten.length; i++){
+			ret += " + " + koeffizienten[i] + "x^" + i;
 		}
 		return ret;
 	}
